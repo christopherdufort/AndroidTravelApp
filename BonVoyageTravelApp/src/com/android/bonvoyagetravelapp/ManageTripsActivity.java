83027983abdde0e4090b3be535df31c26bafbde1
1332215 <1332215@2F14-04.ad.dawsoncollege.qc.ca>
2015-12-08 14:06:48 -0500

    FUCKING SYNC WONT WORK

--- BonVoyageTravelApp/src/com/android/bonvoyagetravelapp/ManageTripsActivity.java
+++ BonVoyageTravelApp/src/com/android/bonvoyagetravelapp/ManageTripsActivity.java
@@ -1,11 +1,15 @@
 package com.android.bonvoyagetravelapp;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,41 +37,41 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ManageTripsActivity extends Activity {
-	
+
 	private static DBHelper dbh;
 	private ListView lv;
 	private TextView tv;
 	private SimpleCursorAdapter sca;
 	private Cursor cursor;
-	
+
 	private SharedPreferences prefs;
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_manage_trips);
-		dbh=DBHelper.getDBHelper(this);
-		
+		dbh = DBHelper.getDBHelper(this);
+
 		prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		//Custom ownership feel show current users name.
-		tv = (TextView) findViewById(R.id.all_trips_name);	
+		// Custom ownership feel show current users name.
+		tv = (TextView) findViewById(R.id.all_trips_name);
 		tv.setText(prefs.getString("name", ""));
 
 		// Setup multiple unique click events available trips displayed.
 		setUpListeners();
 	}
+
 	/**
 	 * Overwritten onResume() life cycle method called to renew resources.
 	 */
 	@Override
 	public void onResume() {
 		super.onResume();
-		//updateView();
+		// updateView();
 	}
-	
+
 	private void updateView() {
-		
+
 		// change cursor so the data view will be updated
 		cursor = dbh.getAllTrips();
 
@@ -76,8 +80,9 @@ public class ManageTripsActivity extends Activity {
 		sca.changeCursor(cursor);
 
 		// Adapter tell the observers of change in data set.
-		sca.notifyDataSetChanged();	
+		sca.notifyDataSetChanged();
 	}
+
 	/**
 	 * Overwritten onPause() life cycle method called to release resources.
 	 */
@@ -85,67 +90,73 @@ public class ManageTripsActivity extends Activity {
 	public void onPause() {
 		super.onPause();
 		// Release resources.
-		//dbh.close();
+		// dbh.close();
 	}
-	
+
 	/**
 	 * This method is called to set up the simple cursor adapter and link it to
 	 * the DB cursor. This method is also responsible for setting up event
-	 * listeners on those list items. Short click will display details of a trip.
+	 * listeners on those list items. Short click will display details of a
+	 * trip.
 	 */
 	private void setUpListeners() {
-		lv =(ListView) findViewById(R.id.listViewAllTrips);
-		
-		String[] from = { DBHelper.COLUMN_NAME, DBHelper.COLUMN_DESCRIPTION};
-		int[] to = { R.id.text_view_trip_name, R.id.text_view_trip_description};
-		
+		lv = (ListView) findViewById(R.id.listViewAllTrips);
+
+		String[] from = { DBHelper.COLUMN_NAME, DBHelper.COLUMN_DESCRIPTION };
+		int[] to = { R.id.text_view_trip_name, R.id.text_view_trip_description };
+
 		Cursor cursor = dbh.getAllTrips();
 
-		sca = new SimpleCursorAdapter(this, R.layout.activity_manage_trips_list, cursor,from,to,0);
-	
+		sca = new SimpleCursorAdapter(this, R.layout.activity_manage_trips_list, cursor, from, to, 0);
+
 		lv.setAdapter(sca);
-		
-		// Event listener for short clicks will open details of trip in new activity.
+
+		// Event listener for short clicks will open details of trip in new
+		// activity.
 
 		lv.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent,View view, int position, long id) {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				Cursor cursortemp = (Cursor) parent.getItemAtPosition(position);
 
-				int tripId= cursortemp.getInt(0);
+				int tripId = cursortemp.getInt(0);
 				String tripName = cursortemp.getString(5);
-				
+
 				SharedPreferences.Editor editor = prefs.edit();
 				editor.putInt("CURRENTTRIP", tripId);
 				editor.putString("CURRENTTRIPNAME", tripName);
 				editor.commit();
-				
-				Intent intent = new Intent(getApplicationContext(),ItineraryActivity.class);
+
+				Intent intent = new Intent(getApplicationContext(), ItineraryActivity.class);
 				intent.putExtra("MANAGE", true);
 				startActivity(intent);
 			}
 		});
 	}
-	
+
 	/**
-	 * syncTask is an event handler for the sync trip button in the manage trip activity.
-	 * This method is responsible for updating the local SQLite db with changes made server side.
+	 * syncTask is an event handler for the sync trip button in the manage trip
+	 * activity. This method is responsible for updating the local SQLite db
+	 * with changes made server side.
 	 * 
 	 * @param view
-	 * 			the view that triggered this event handler (button click)
+	 *            the view that triggered this event handler (button click)
 	 */
 	public void syncTrips(View view) {
 		Toast toast = Toast.makeText(this, "Syncing with server", Toast.LENGTH_SHORT);
 		toast.show();
 		launchSyncAPITrips();
 	}
+
 	private void launchSyncAPITrips() {
-		String url = "travel-bonvoyage.rhcloud.com/apiTrips?email=" + prefs.getString("email", "") + "&password="+ prefs.getString("password", "");
+		String url = "http://travel-bonvoyage.rhcloud.com/apiTrips";
+		String jsonData = "{\"email\":\"" + prefs.getString("email", "") + "\",\"password\":\""
+				+ prefs.getString("password", "") + "\"}";
 
 		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
 		if (networkInfo != null && networkInfo.isConnected()) {
-			new DownloadTripsData().execute(url);
+			new DownloadTripsData().execute(url, jsonData);
 		}
 	}
 
@@ -153,58 +164,99 @@ public class ManageTripsActivity extends Activity {
 
 		@Override
 		protected String doInBackground(String... params) {
-
+			Log.d("debug", "IM IN A BACKGROUND");
 			InputStream input = null;
+			OutputStream output;
 			HttpURLConnection conn = null;
-			String result = null;
+			String result = "";
 
 			try {
+				byte[] jsonBytes = params[1].getBytes("UTF-8");
+				Integer jsonBytesLength = jsonBytes.length;
+
 				URL url = new URL(params[0]);
 				// create and open the connection
 				conn = (HttpURLConnection) url.openConnection();
 
-				conn.setRequestMethod("GET");
+				if (conn == null)
+					Log.d("debug", "connection is null");
+				conn.setRequestMethod("POST");
+				//conn.setRequestMethod("GET");
 
 				// specifies whether this connection allows receiving data
 				conn.setDoInput(true);
+				conn.setDoOutput(true);
 
-				// Starts the query
-				// Actually sends request
-				conn.connect();
+				conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+				
+				
+				//output = new BufferedOutputStream(conn.getOutputStream());
+				
+				//Create JSONObject here
+				JSONObject jsonParam = new JSONObject();
+				jsonParam.put("email", prefs.getString("email", ""));
+				jsonParam.put("password", prefs.getString("password", ""));
 
-				int responseCode = conn.getResponseCode();
-
-				if (responseCode != HttpURLConnection.HTTP_OK)
+				String str = jsonParam.toString();
+				byte[] data=str.getBytes("UTF-8");
+				conn.addRequestProperty("Content-Length", jsonParam.toString());
+				Log.d("debug", "I DIDID WRITE YET");
+				DataOutputStream printout = new DataOutputStream(conn.getOutputStream ());
+				printout.write(data);
+				printout.flush();
+				printout.close();
+				//output.write();
+				//output.flush();
+				//output.close();
+				//conn.connect();
+				Log.d("debug", ""+ conn.getResponseCode());
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_OK)
+				{
+					Log.d("debug", "IM OUT OF HERE");
+					Log.d("debug", ""+ conn.getResponseCode());
 					return "";
+				}
 
 				// get the stream for the data from the website
 				input = conn.getInputStream();
+				
+				if (input == null)
+					Log.d("debug", "input is null");
+				else
+					Log.d("debug", "input is NOT null");
 
 				// read the stream
-				char[] buffer = new char[32000];
 				BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
-				reader.read(buffer);
-
-				// goes to onPOstExecute()
-				result = new String(buffer);
-
+				
+				
+				String line = "";  
+				while ((line = reader.readLine()) != null) {  
+					result += line;
+				}
+				Log.d("debug", result);
 			} catch (IOException e) {
 				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} finally {
-				conn.disconnect();
 				try {
-					input.close();
+					if (input != null)
+						input.close();
 				} catch (IOException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				if (conn!= null)
+					conn.disconnect();
 			}
 			return result;
 		}
-		
+
 		@Override
 		protected void onPostExecute(String result) {
 			if (result.equals("")) {
-				//cant connect make a toast
+				// cant connect make a toast
 			} else {
 				parseResults(result);
 			}
@@ -217,28 +269,31 @@ public class ManageTripsActivity extends Activity {
 			JSONObject jsonObj = new JSONObject(result);
 
 			Log.d("json", jsonObj.toString());
-			
-			/*JSONObject sysObj = jsonObj.getJSONObject("sys");
-			country = sysObj.getString("country");
 
-			nameOfArea = jsonObj.getString("name");
-
-			JSONArray array = jsonObj.getJSONArray("weather");
-			descriptionForecast = array.getJSONObject(0).getString("description");
-			mainForecast = array.getJSONObject(0).getString("main");
-
-			JSONObject weatherObj = jsonObj.getJSONObject("wind");
-			windSpeed = "" + weatherObj.getDouble("speed");
-
-			JSONObject mainObj = jsonObj.getJSONObject("main");
-			humidity = "" + mainObj.getInt("humidity");
-			pressure = "" + mainObj.getInt("pressure");
-			temp = "" + mainObj.getDouble("temp");
-			minTemp = "" + mainObj.getDouble("temp_min");
-			maxTemp = "" + mainObj.getDouble("temp_max");*/
+			/*
+			 * JSONObject sysObj = jsonObj.getJSONObject("sys"); country =
+			 * sysObj.getString("country");
+			 * 
+			 * nameOfArea = jsonObj.getString("name");
+			 * 
+			 * JSONArray array = jsonObj.getJSONArray("weather");
+			 * descriptionForecast =
+			 * array.getJSONObject(0).getString("description"); mainForecast =
+			 * array.getJSONObject(0).getString("main");
+			 * 
+			 * JSONObject weatherObj = jsonObj.getJSONObject("wind"); windSpeed
+			 * = "" + weatherObj.getDouble("speed");
+			 * 
+			 * JSONObject mainObj = jsonObj.getJSONObject("main"); humidity = ""
+			 * + mainObj.getInt("humidity"); pressure = "" +
+			 * mainObj.getInt("pressure"); temp = "" +
+			 * mainObj.getDouble("temp"); minTemp = "" +
+			 * mainObj.getDouble("temp_min"); maxTemp = "" +
+			 * mainObj.getDouble("temp_max");
+			 */
 
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-	} //end of parseResults
+	} // end of parseResults
 }
