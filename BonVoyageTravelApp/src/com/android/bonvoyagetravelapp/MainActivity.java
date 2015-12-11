f123b9c99e5cefdda5a638f2b65bf3f5a9192be9
Irina <Irina@Irina>
2015-12-10 20:50:33 -0500

    main activity import clean-up

--- BonVoyageTravelApp/src/com/android/bonvoyagetravelapp/MainActivity.java
+++ BonVoyageTravelApp/src/com/android/bonvoyagetravelapp/MainActivity.java
@@ -1,29 +1,13 @@
 package com.android.bonvoyagetravelapp;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.util.Date;
-
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.location.Location;
-import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,15 +19,11 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	private SharedPreferences prefs;
-	private DBHelper dbh;
-	private Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		dbh = DBHelper.getDBHelper(this);
-		context = this;
 
 		prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
